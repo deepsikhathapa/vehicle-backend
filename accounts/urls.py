e42334aab86b7cb0670107446955c41399402280
deeps <deepseekathapa@gmail.com>
2026-03-08 12:21:50 +0545

    fix: login issue

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,7 +10,7 @@ from .admin_views import AdminListView, AdminCostumerListView, AdminVendorListVi
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('signin/', MyTokenObtainPairView.as_view(), name='login'),
+    path('login/', MyTokenObtainPairView.as_view(), name='login'),
     path('refresh/', TokenRefreshView.as_view(), name='refresh'),
 
     path('me/', ProfileView.as_view(), name='me'),
